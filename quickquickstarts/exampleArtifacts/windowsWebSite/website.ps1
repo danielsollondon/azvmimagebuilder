d98c98b8f3d702487cc32df52f40a39581da3182
Daniel Sol <danis@microsoft.com>
2019-05-02 18:19:17 -0700

    sample

---
+++ quickquickstarts/exampleArtifacts/windowsWebSite/website.ps1
@@ -0,0 +1,14 @@
+# Install IIS Website
+Install-WindowsFeature -name Web-Server -IncludeManagementTools
+
+# Setup directory
+New-Item C:\ImageBuilderWebApp -type Directory
+
+Set-Content C:\ImageBuilderWebApp\Default.htm "ImageBuilderWebApp Default Page"
+
+New-Item IIS:\AppPools\DemoAppPool
+
+Import-Module "WebAdministration"
+
+New-Item IIS:\Sites\ImageBuilderWebApp -physicalPath C:\ImageBuilderWebApp -bindings @{protocol="http";bindingInformation=":8080:"}
+Set-ItemProperty IIS:\Sites\ImageBuilderWebApp -name applicationPool -value DemoAppPool
