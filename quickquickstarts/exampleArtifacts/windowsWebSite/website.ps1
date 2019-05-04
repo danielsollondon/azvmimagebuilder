abbf2153f5221890ff8d033e8b8bb857aedebc9d
Daniel Sol <danis@microsoft.com>
2019-05-04 07:25:36 -0700

    bug in ps

--- quickquickstarts/exampleArtifacts/windowsWebSite/website.ps1
+++ quickquickstarts/exampleArtifacts/windowsWebSite/website.ps1
@@ -6,9 +6,4 @@ New-Item C:\ImageBuilderWebApp -type Directory
 
 Set-Content C:\ImageBuilderWebApp\Default.htm "ImageBuilderWebApp Default Page"
 
-New-Item C:\AppPools\DemoAppPool -type Directory
-
-Import-Module "WebAdministration"
-
-New-Item IIS:\Sites\ImageBuilderWebApp -physicalPath C:\ImageBuilderWebApp -bindings @{protocol="http";bindingInformation=":8080:"} -force
-Set-ItemProperty IIS:\Sites\ImageBuilderWebApp -name applicationPool -value DemoAppPool
+New-Item IIS:\Sites\ImageBuilderWebApp -physicalPath C:\ImageBuilderWebApp -bindings @{protocol="http";bindingInformation=":8080:"}
