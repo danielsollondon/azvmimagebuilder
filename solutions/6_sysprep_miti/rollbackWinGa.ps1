9f90db59c10bd0bdef75574fe35ae11516c70ac4
Daniel Sol <>
2020-10-26 15:26:40 -0700

    workaround v3 for sprep

--- solutions/6_sysprep_miti/rollbackWinGa.ps1
+++ solutions/6_sysprep_miti/rollbackWinGa.ps1
@@ -1,11 +1,23 @@
-Write-Host 'v2 Start of mitigation sysprep for 26th October sysprep issue, this **must** not be used past mid November'
-$myApp=Get-WmiObject -Query "SELECT * FROM Win32_Product WHERE Name Like '%Windows Azure VM Agent%'"
+Write-Host 'v3 Start of mitigation sysprep for 26th October sysprep issue, this **must** not be used past mid November'
+#disable WinGA services
+Set-Service -Name RdAgent -StartupType Disabled
+Set-Service -Name WindowsAzureGuestAgent -StartupType Disabled
 
-$MyApp.Uninstall() 
-Write-Host "stop service - to stop auto uopdate until image create"
-Get-Service -DisplayName "RdAgent" | Stop-Service 
+# remove the registry key for sysprep
+$path = "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Setup\SysPrepExternal\Generalize"
+$generalizeKey = Get-Item -Path $path
+$generalizeProperties = $generalizeKey | Select-Object -ExpandProperty property
 
-Write-Host "End of mitigation"
+$values = $generalizeProperties | ForEach-Object {
+New-Object psobject -Property @{"Name"=$_;
+"Value" = (Get-ItemProperty -Path $path -Name $_).$_}
+}
 
-
+$values | ForEach-Object {
+$item = $_;
+if( $item.Value.Contains("VMAgentDisabler.dll")) {
+Write-HOST "Removing " $item.Name - $item.Value;
+Remove-ItemProperty -Path $path -Name $item.Name;
+}
+}
 
