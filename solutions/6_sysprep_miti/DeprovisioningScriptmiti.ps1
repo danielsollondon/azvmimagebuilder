c01d303cc224e2edeadd6ecaa38be010ed481d73
Daniel Sol <>
2020-10-26 09:50:01 -0700

    workaround for sprep

--- solutions/6_sysprep_miti/DeprovisioningScriptmiti.ps1
+++ solutions/6_sysprep_miti/DeprovisioningScriptmiti.ps1
@@ -1,3 +1,4 @@
+Write-Host 'Modified version of sysprep for 26th October sysprep issue, this should must be used past mid November'
 Write-Output '>>> Waiting for GA Service (WindowsAzureTelemetryService) to start ...'
 while ((Get-Service WindowsAzureTelemetryService) -and ((Get-Service WindowsAzureTelemetryService).Status -ne 'Running')) { Start-Sleep -s 5 }
 Write-Output '>>> Waiting for GA Service (WindowsAzureGuestAgent) to start ...'
