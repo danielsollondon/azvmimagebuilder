c86d9d4062ff0f9a8d9ad881f71ebcabc397aadd
Daniel Sol <>
2021-01-14 15:00:52 -0800

    intrumentation fort access denied in REG UNLOAD

--- solutions/14_Building_Images_WVD/1_Optimize_OS_for_WVD.ps1
+++ solutions/14_Building_Images_WVD/1_Optimize_OS_for_WVD.ps1
@@ -1,21 +1,30 @@
- # OS Optimizations for WVD
- write-host 'AIB Customization: OS Optimizations for WVD'
- $appName = 'optimize'
- $drive = 'C:\'
- New-Item -Path $drive -Name $appName  -ItemType Directory -ErrorAction SilentlyContinue
- $LocalPath = $drive + '\' + $appName 
- set-Location $LocalPath
- $osOptURL = 'https://github.com/The-Virtual-Desktop-Team/Virtual-Desktop-Optimization-Tool/archive/master.zip'
- $osOptURLexe = 'Windows_10_VDI_Optimize-master.zip'
- $outputPath = $LocalPath + '\' + $osOptURLexe
- Invoke-WebRequest -Uri $osOptURL -OutFile $outputPath
- write-host 'AIB Customization: Starting OS Optimizations script'
- Expand-Archive -LiteralPath 'C:\\Optimize\\Windows_10_VDI_Optimize-master.zip' -DestinationPath $Localpath -Force -Verbose
- Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force -Verbose
+# OS Optimizations for WVD
+write-host 'AIB Customization: OS Optimizations for WVD'
+$appName = 'optimize'
+$drive = 'C:\'
+New-Item -Path $drive -Name $appName  -ItemType Directory -ErrorAction SilentlyContinue
+$LocalPath = $drive + '\' + $appName 
+set-Location $LocalPath
+$osOptURL = 'https://github.com/The-Virtual-Desktop-Team/Virtual-Desktop-Optimization-Tool/archive/master.zip'
+$osOptURLexe = 'Windows_10_VDI_Optimize-master.zip'
+$outputPath = $LocalPath + '\' + $osOptURLexe
+Invoke-WebRequest -Uri $osOptURL -OutFile $outputPath
+write-host 'AIB Customization: Starting OS Optimizations script'
+Expand-Archive -LiteralPath 'C:\\Optimize\\Windows_10_VDI_Optimize-master.zip' -DestinationPath $Localpath -Force -Verbose
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force -Verbose
+Set-Location -Path C:\\Optimize\\Virtual-Desktop-Optimization-Tool-master
 
- # Patch: overide the Win10_VirtualDesktop_Optimize.ps1 - setting 'Set-NetAdapterAdvancedProperty'(see readme.md)
- $updatePath= "C:\optimize\Virtual-Desktop-Optimization-Tool-master\Win10_VirtualDesktop_Optimize.ps1"
- ((Get-Content -path $updatePath -Raw) -replace 'Set-NetAdapterAdvancedProperty -DisplayName "Send Buffer Size" -DisplayValue 4MB','#Set-NetAdapterAdvancedProperty -DisplayName "Send Buffer Size" -DisplayValue 4MB') | Set-Content -Path $updatePath
+# instrumentation
+$osOptURL = 'https://raw.githubusercontent.com/danielsollondon/azvmimagebuilder/master/solutions/14_Building_Images_WVD/'
+$osOptURLexe = 'optimize.ps1'
+$outputPath = $LocalPath + '\' + $osOptURLexe
+Invoke-WebRequest -Uri $osOptURL -OutFile $outputPath
+
+
+
+# Patch: overide the Win10_VirtualDesktop_Optimize.ps1 - setting 'Set-NetAdapterAdvancedProperty'(see readme.md)
+$updatePath= "C:\optimize\Virtual-Desktop-Optimization-Tool-master\Win10_VirtualDesktop_Optimize.ps1"
+((Get-Content -path $updatePath -Raw) -replace 'Set-NetAdapterAdvancedProperty -DisplayName "Send Buffer Size" -DisplayValue 4MB','#Set-NetAdapterAdvancedProperty -DisplayName "Send Buffer Size" -DisplayValue 4MB') | Set-Content -Path $updatePath
 
 # Patch: overide the REG UNLOAD, needs GC before, otherwise will Access Deny unload(see readme.md)
 
@@ -32,8 +41,8 @@ Set-Content $updatePath $file
 
 
 # run script
- Set-Location -Path C:\\Optimize\\Virtual-Desktop-Optimization-Tool-master
- .\Win10_VirtualDesktop_Optimize.ps1 -WindowsVersion 2004 -Verbose
+.\optimize -WindowsVersion 2004 -Verbose
+ #.\Win10_VirtualDesktop_Optimize.ps1 -WindowsVersion 2004 -Verbose
  write-host 'AIB Customization: Finished OS Optimizations script'
 
 
