23d7d9902d4030fd02a1d5a08fe33ef51452d3b6
Daniel Sol <>
2021-03-03 10:15:56 -0800

    bug fixes

--- solutions/1_Azure_DevOps/6_sysprep_miti/rollbackWinGa.ps1
+++
@@ -1,8 +0,0 @@
- ## download older version of agent
- Invoke-WebRequest -Uri https://go.microsoft.com/fwlink/?LinkID=394789 -OutFile guestagent.msi -UseBasicParsing
-
- ## installs C:\WindowsAzure\Packages_20201026_144227
- Start-Process msiexec.exe -Wait -ArgumentList '/i guestagent.msi /qn /norestart'
- 
- ## stop service - to stop auto uopdate until image create
- Get-Service -DisplayName "RdAgent" | Stop-Service 
