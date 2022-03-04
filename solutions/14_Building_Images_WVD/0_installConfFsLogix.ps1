a12c1f71868593356d14c508e3f0166e70bdc27e
Henry Gelderbloem <henry.gelderbloem@waterstons.com>
2022-03-04 10:56:58 +0000

    Updated FsLogix installer to ensure that FsLogix is updated if the currently available version is out of date.

--- solutions/14_Building_Images_WVD/0_installConfFsLogix.ps1
+++ solutions/14_Building_Images_WVD/0_installConfFsLogix.ps1
@@ -1,20 +1,46 @@
-write-host 'AIB Customization: Downloading FsLogix'
-New-Item -Path C:\\ -Name fslogix -ItemType Directory -ErrorAction SilentlyContinue
-$LocalPath = 'C:\\fslogix'
-$WVDflogixURL = 'https://raw.githubusercontent.com/DeanCefola/Azure-WVD/master/PowerShell/FSLogixSetup.ps1'
-$WVDFslogixInstaller = 'FSLogixSetup.ps1'
-$outputPath = $LocalPath + '\' + $WVDFslogixInstaller
-Invoke-WebRequest -Uri $WVDflogixURL -OutFile $outputPath
-set-Location $LocalPath
-
+Write-Host 'AIB Customization: Downloading FsLogix'
 $fsLogixURL="https://aka.ms/fslogix_download"
 $installerFile="fslogix_download.zip"
+New-Item `
+    -Path C:\Temp `
+    -Name fslogix `
+    -ItemType Directory `
+    -ErrorAction SilentlyContinue
+$LocalPath = 'C:\Temp\fslogix'
+(New-Object System.Net.WebClient).DownloadFile("$fsLogixURL","$LocalPath\$installerFile")
+Expand-Archive `
+    -Path $LocalPath\$installerFile `
+    -DestinationPath $LocalPath `
+    -Verbose
+Write-Host 'AIB Customization: Downloading of FsLogix installer finished'
 
-Invoke-WebRequest $fsLogixURL -OutFile $LocalPath\$installerFile
-Expand-Archive $LocalPath\$installerFile -DestinationPath $LocalPath
-write-host 'AIB Customization: Download Fslogix installer finished'
+Write-Host 'AIB Customization: Comparing FsLogix versions'
+$downloadedFsLogixVersion = Get-Item $LocalPath\x64\Release\FSLogixAppsSetup.exe | Select-Object VersionInfo
+Write-Host 'AIB Customization: Downloaded version number:' $downloadedFsLogixVersion.VersionInfo.FileVersion
 
-write-host 'AIB Customization: Start Fslogix installer'
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force -Verbose
-.\\FSLogixSetup.ps1 -ProfilePath \\wvdSMB\wvd -Verbose 
-write-host 'AIB Customization: Finished Fslogix installer' 
+$installedFsLogixVersion = Get-Item "C:\Program Files\FSLogix\Apps\frx.exe" | Select-Object VersionInfo
+Write-Host 'AIB Customization: Installed version number:' $installedFSLogixVersion.VersionInfo.FileVersion
+
+if ([version]$downloadedFsLogixVersion.VersionInfo.FileVersion -gt [version]$installedFsLogixVersion.VersionInfo.FileVersion) {
+    Write-Host 'AIB Customization: Downloaded version is greator than that installed. Updating FsLogix.'
+    Write-Host 'AIB Customization: Uninstalling FsLogix'
+    Start-Process `
+    -FilePath $LocalPath\x64\Release\FSLogixAppsSetup.exe `
+    -ArgumentList "/uninstall /quiet /norestart" `
+    -Wait `
+    -Passthru
+
+    Write-Host 'AIB Customization: Starting Fslogix installer'
+    Start-Process `
+    -FilePath $LocalPath\x64\Release\FSLogixAppsSetup.exe `
+    -ArgumentList "/install /quiet" `
+    -Wait `
+    -Passthru
+} else {
+    Write-Host 'AIB Customization: Installed version matches the downloaded version. Skipping FsLogix update.'
+}
+
+$installedFsLogixVersion = Get-Item "C:\Program Files\FSLogix\Apps\frx.exe" | Select-Object VersionInfo
+Write-Host 'AIB Customization: Installed version number is now:' $installedFSLogixVersion.VersionInfo.FileVersion
+
+Write-Host 'AIB Customization: Finished Fslogix installer'
